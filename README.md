6b39d779d3a87b0b47d8c35b9898d455b2e0f3b1
dreamcatcher696 <thomaseckert@MacBook-Pro-van-Thomas.local>
2018-10-24 16:53:37 +0200

    update readme+ add functionality to main.cpp (color;canvas)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 2018_beeldinterpretatie_Eckert_Thomas
 labo beeldverwerking KU Leuven
+| parameter | doel|
+|--------|--------|
+|-h|help|
+|-ig|required! locatie van image in grijswaarden|
+|-ic|required! locatie van image in kleur|
+
+de applicatie gaat door alle stappen van de gevraagde opgave, om naar het volgend scherm te gaan, gelieve op eender welke knop (behalve de powerknop) te drukken.
--- sessie0/sessie0/main.cpp
+++ sessie0/sessie0/main.cpp
@@ -15,6 +15,7 @@ int main(int argc, const char** argv)
     CommandLineParser parser(argc, argv,
         "{help h usage ?|       |show this message}"
         "{image_gray ig |       |(required) path to image}"
+        "{image_color ic|       |(required) path to color image}"
 
     );
     //if help
@@ -27,45 +28,78 @@ int main(int argc, const char** argv)
     // collecting data from parameters
     string image_gray_location(parser.get<string>("image_gray"));
     if(image_gray_location.empty()){
-    cerr<<"check parameters, no location given" <<endl;
+    cerr<<"check parameters, no gray location given" <<endl;
         parser.printMessage();
         return -1;
     }
 
-    //create imagematrix
-    Mat image;
-    image = imread(image_gray_location);
+    string image_color_location(parser.get<string>("image_color"));
+    if(image_gray_location.empty()){
+        cerr<<"check parameters, no color location given" <<endl;
+        parser.printMessage();
+        return -1;
+    }
+
+    //create imagematrix for gray image
+    Mat grayimage;
+    grayimage = imread(image_gray_location);
     //image found?
-    if(image.empty())
+    if(grayimage.empty())
     {
-        cerr<<"no image found, please try again with correct location"<<endl;
+        cerr<<"no gray image found, please try again with correct location"<<endl;
+        return -1;
+    }
+
+    Mat colorimage;
+    colorimage = imread(image_color_location);
+    //image found?
+    if(colorimage.empty())
+    {
+        cerr<<"no color image found, please try again with correct location"<<endl;
         return -1;
     }
     //show image
-    imshow("foto", image);
+    imshow("foto", grayimage);
     //wait till key is pressed
     waitKey(0);
+    imshow("foto2",colorimage);
+    waitKey(0);
     vector <Mat>channels;
-    split(image,channels);
+    split(colorimage,channels);
     //3 kanalen showen
 
+    imshow("red", channels[2]);
+    waitKey(0);
+    imshow("green", channels[1]);
+    waitKey(0);
 
-    Mat image_gryscale
-    cvtcolor(image,image_grayscale, COLOR_BGR2GRAY);
-    imshow();
+    imshow("blue", channels[0]);
+    waitKey(0);
 
-    for(int row = 0; row < image.rows;row++)
+
+    //foto in grijswaarden tonen
+    Mat image_grayscale;
+    cvtColor(colorimage,image_grayscale, COLOR_BGR2GRAY);
+    imshow("grijswaarden", image_grayscale);
+    waitKey(0);
+
+    for(int row = 0; row < grayimage.rows;row++)
     {
-        for(int col = 0;col < image.cols;col++)
+        for(int col = 0;col < grayimage.cols;col++)
         {
-            int value = image.at<uchar>(row,col);
+            int value = grayimage.at<uchar>(row,col);
             cerr<<value<< " ";
-            cerr << (int)image.at<uchar>(row,col) << " ";
+            cerr << (int)colorimage.at<uchar>(row,col) << " ";
         }
     }
     cerr<<endl;
-
-    Mat canvas = Mat::zeros(Size(250));
+    //lege canvas maken
+    Mat canvas = Mat::zeros(250, 250, CV_8UC3);
+    //rechthoek tekenen
+    rectangle(canvas, Point(50,50), Point(200,200), Scalar(255,0,0),1);
+    circle(canvas, Point(125,125),50,Scalar(0,255,0), 2);
+    imshow("Canvas", canvas);
+    waitKey(0);
     return 0;
 }
 
