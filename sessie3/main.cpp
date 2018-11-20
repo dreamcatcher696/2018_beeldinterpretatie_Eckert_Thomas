ad5933a9aab436ae49005715eaa5ce1fe0b3d816
Thomas Eckert <thomas.eckert@hotmail.com>
2018-11-20 09:36:12 +0100

    multiple detection kinda working needs tweaking

--- sessie3/main.cpp
+++ sessie3/main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 using namespace cv;
 
 int match_method;
+int amount_matches = 0;
+int threshval;
 Mat grayInput;
 Mat grayTemplate;
+Mat input;
 const char* image_window = "source Image";
 const char* result_window = "result window";
 
@@ -22,6 +25,7 @@ int main(int argc, const char** argv) {
                              "{help h usage ?|       | show this message| in case of MACOS:DO NOT MOVE WINDOW}"
                              "{@template|  | (required) path to template}"
                              "{@input| |(required) path to input inmage}"
+                             "{(f)indall f | | use f to find all matches instead of best match}"
     );
     //if help
     if (parser.has("help")) {
@@ -51,7 +55,7 @@ int main(int argc, const char** argv) {
         return -1;
     }
     //read in template
-    Mat input = imread(input_path);      //template1 cause template is in use by c++
+    input = imread(input_path);      //template1 cause template is in use by c++
     if (input.empty()) {
         cerr << "could not read input" << endl;
         parser.printMessage();
@@ -64,7 +68,15 @@ int main(int argc, const char** argv) {
     namedWindow(image_window,WINDOW_AUTOSIZE);
     namedWindow(result_window,WINDOW_AUTOSIZE);
     const char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM_CCORR \n 3: TM CCORR NORMED \n 4:TM COEFF\n 5: TM COEFF NORMED";
+    const char* amount_of_matches = "0: only single match \n 1: all matches";
+
     createTrackbar(trackbar_label, result_window, &match_method, 5, MatchingMethod);
+    if(parser.has("f"))
+    {
+        createTrackbar(amount_of_matches, result_window, &amount_matches,1, MatchingMethod);
+        createTrackbar("Threshold", result_window, &threshval, 100, MatchingMethod);
+    }
+
 
     MatchingMethod(0,0);
     waitKey(0);
@@ -75,44 +87,51 @@ void MatchingMethod(int, void*)
 {
     Mat result;
 
-
-
     matchTemplate(grayInput, grayTemplate, result, match_method);
     normalize(result, result, 0,1,NORM_MINMAX, -1, Mat());
-
-    double minVal,maxVal;
-    Point minLoc, maxLoc;
-    Point matchLoc;
-
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-    if(match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+    if(amount_matches==0)
     {
-        matchLoc = minLoc;
+        double minVal,maxVal;
+        Point minLoc, maxLoc;
+        Point matchLoc;
 
+        minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+        if(match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+        {
+            matchLoc = minLoc;
+
+        }
+        else{
+            matchLoc = maxLoc;
+        }
+
+        Mat temp = grayInput.clone();
+        rectangle(temp,matchLoc,Point(matchLoc.x + grayTemplate.cols, matchLoc.y + grayTemplate.rows), Scalar::all(0),2,8,0);
+        imshow(result_window, temp);
+        waitKey(0);
     }
-    else{
-        matchLoc = maxLoc;
+    else
+    {
+        Mat mask;
+        inRange(result, ((float)threshval/255),1,mask);
+        imshow("Threshold", mask);
+
+        Mat temp;
+        input.copyTo(temp);
+        vector<vector<Point>> contours;
+        findContours(mask,contours,RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        for(int i=0;i<contours.size();i++)
+        {
+            Rect region = boundingRect(contours[i]);
+            Mat temp2 = result(region);
+            Point maxLoc;
+
+            minMaxLoc(temp2,nullptr,nullptr,nullptr,&maxLoc);
+            rectangle(temp, Point(region.x+maxLoc.x, region.y+maxLoc.y), Point(maxLoc.x+region.x+grayTemplate.cols, maxLoc.y+region.y+grayTemplate.rows),Scalar(0,255,0), 2,8,0);
+        }
+        imshow(result_window, temp);
+        waitKey(0);
     }
 
-    Mat temp = grayInput.clone();
-    rectangle(temp,matchLoc,Point(matchLoc.x + grayTemplate.cols, matchLoc.y + grayTemplate.rows), Scalar::all(0),2,8,0);
-    //rectangle(result,matchLoc,Point(matchLoc.x + grayTemplate.cols, matchLoc.y + grayTemplate.rows), Scalar::all(0),2,8,0);
-    //imshow("template", grayTemplate);
-    //imshow("normalised", result);
-    imshow(result_window, temp);
-
-    Mat mask, temp2;
-    threshold(result, mask,0.9,1,THRESH_BINARY);
-    mask.convertTo(mask,CV_8UC1);
-    mask *= 255;
-    imshow("thresholded mask", mask);
-
-    temp2 = grayInput.clone();
-
-
-
-
-    imshow(result_window, temp);
-    waitKey(0);
     return;
 }
